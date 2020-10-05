e7d030556b27aba31f798e65c7670849cba50d18
neo-sumeet <sumeet_singh@hotmail.com>
2020-10-04 23:30:37 -0700

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+VALUES (1001,'USD','INR',65.0,0);
+INSERT into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+VALUES (1002,'EUR','INR',75.0,0);
+INSERT into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+VALUES (1003,'AUD','INR',25.0,0);
